198469a151141bd2f2556c19aad0cab9a3ea0aa3
Bob German <bgerman1@hotmail.com>
2022-06-02 18:22:12 -0400

    Fix excess initialize() calls B02 and B03

--- src/create-core-app/bespoke/B03-after-teams-sso/client/identity/teamsLoginLauncher.js
+++ src/create-core-app/bespoke/B03-after-teams-sso/client/identity/teamsLoginLauncher.js
@@ -1,9 +1,10 @@
 import 'https://res.cdn.office.net/teams-js/2.0.0/js/MicrosoftTeams.min.js';
+import { ensureTeamsSdkInitialized } from '../modules/teamsHelpers.js';
 
 const teamsLoginLauncherButton = document.getElementById('teamsLoginLauncherButton');
 
 teamsLoginLauncherButton.addEventListener('click', async ev => {
-   await microsoftTeams.initialize();
+   await ensureTeamsSdkInitialized();
    await microsoftTeams.authentication.authenticate({
       url: `${window.location.origin}/identity/login.html?teams=true`,
       width: 600,
--- src/create-core-app/bespoke/B03-after-teams-sso/client/modules/teamsHelpers.js
+++ src/create-core-app/bespoke/B03-after-teams-sso/client/modules/teamsHelpers.js
@@ -1,11 +1,19 @@
 import 'https://res.cdn.office.net/teams-js/2.0.0/js/MicrosoftTeams.min.js';
 
+let teamsInitPromise;
+export function ensureTeamsSdkInitialized() {
+    if (!teamsInitPromise) {
+        teamsInitPromise = microsoftTeams.app.initialize();
+    }
+    return teamsInitPromise;
+}
+
 // async function returns true if we're running in Teams
 export async function inTeams() {
     try {
-        await microsoftTeams.app.initialize();
+        await ensureTeamsSdkInitialized();
         const context = await microsoftTeams.app.getContext();
-        return (context.app.host.name === microsoftTeams.HOST_NAME.teams);
+        return (context.app.host.name === microsoftTeams.HostName.teams);
     }
     catch (e) {
         console.log(`${e} from Teams SDK, may be running outside of Teams`);    
