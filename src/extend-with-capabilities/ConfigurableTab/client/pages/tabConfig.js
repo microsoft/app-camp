275b63f07efc7429b82ec89223febe5b1352a0b0
Bob German <bgerman1@hotmail.com>
2022-06-03 11:51:43 -0400

    Update ConfigurableTab to SDKv2

--- src/extend-with-capabilities/ConfigurableTab/client/pages/tabConfig.js
+++ src/extend-with-capabilities/ConfigurableTab/client/pages/tabConfig.js
@@ -1,7 +1,6 @@
-import 'https://statics.teams.cdn.office.net/sdk/v1.11.0/js/MicrosoftTeams.min.js';
-import {
-    getLoggedInEmployee
-} from '../identity/identityClient.js';
+import 'https://res.cdn.office.net/teams-js/2.0.0/js/MicrosoftTeams.min.js';
+import { ensureTeamsSdkInitialized } from '../modules/teamsHelpers.js';
+import { getLoggedInEmployee } from '../identity/identityClient.js';
 
 import {
     getCategories
@@ -13,49 +12,50 @@ async function displayUI() {
     const messageDiv = document.getElementById('message');
 
     try {
-        microsoftTeams.initialize(async () => {
 
-            const employee = await getLoggedInEmployee();
-            if (!employee) {
-                // Nobody was logged in, redirect to login page
-                window.location.href = "/identity/aadLogin.html";
-            }
-            let selectedCategoryId = 0;
-            let selectedCategoryName = '';
+        const employee = await getLoggedInEmployee();
+        if (!employee) {
+            // Nobody was logged in, redirect to login page
+            window.location.href = "/identity/aadLogin.html";
+        }
+        let selectedCategoryId = 0;
+        let selectedCategoryName = '';
 
-            if (employee) {
+        if (employee) {
 
-                // Set up the save handler for when they save the config
-                microsoftTeams.settings.registerOnSaveHandler((saveEvent) => {
+            await ensureTeamsSdkInitialized();
+            
+            // Set up the save handler for when they save the config
+            microsoftTeams.settings.registerOnSaveHandler((saveEvent) => {
 
-                    const url = `${window.location.origin}/pages/categoryDetail.html?categoryId=${selectedCategoryId}`;
-                    const entityId = `ProductCategory ${selectedCategoryId}`;
-                    microsoftTeams.settings.setSettings({
-                      "suggestedDisplayName": selectedCategoryName,
-                      "entityId": entityId,
-                      "contentUrl": url,
-                      "websiteUrl": url
-                    });
-                    saveEvent.notifySuccess();
-                   });
-
-                // Populate the dropdown so they can choose a config
-                const categories = await getCategories();
-                categories.forEach((category) => {
-                    const option = document.createElement('option');
-                    option.value = category.categoryId;
-                    option.innerText = category.displayName;
-                    categorySelect.appendChild(option);
+                const url = `${window.location.origin}/pages/categoryDetail.html?categoryId=${selectedCategoryId}`;
+                const entityId = `ProductCategory ${selectedCategoryId}`;
+                microsoftTeams.settings.setSettings({
+                    "suggestedDisplayName": selectedCategoryName,
+                    "entityId": entityId,
+                    "contentUrl": url,
+                    "websiteUrl": url
                 });
+                saveEvent.notifySuccess();
+            });
 
-                // When a category is selected, it's OK to save
-                categorySelect.addEventListener('change', (ev) => {
-                    selectedCategoryName = ev.target.options[ev.target.selectedIndex].innerText;
-                    selectedCategoryId = ev.target.value;
-                    microsoftTeams.settings.setValidityState(true);
-                });
-            }
-        });
+            // Populate the dropdown so they can choose a config
+            const categories = await getCategories();
+            categories.forEach((category) => {
+                const option = document.createElement('option');
+                option.value = category.categoryId;
+                option.innerText = category.displayName;
+                categorySelect.appendChild(option);
+            });
+
+            // When a category is selected, it's OK to save
+            categorySelect.addEventListener('change', (ev) => {
+                selectedCategoryName = ev.target.options[ev.target.selectedIndex].innerText;
+                selectedCategoryId = ev.target.value;
+                microsoftTeams.settings.setValidityState(true);
+            });
+        }
+
     }
     catch (error) {            // If here, we had some other error
         messageDiv.innerText = `Error: ${JSON.stringify(error.message)}`;
