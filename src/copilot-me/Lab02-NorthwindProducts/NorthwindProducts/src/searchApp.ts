fad7dd9089ee97bec09a747eefa8df0d3d8b35d3
Bob German <Bob.German@microsoft.com>
2023-10-17 15:51:47 -0400

    Extracted product search ME

--- src/copilot-me/Lab02-NorthwindProducts/NorthwindProducts/src/searchApp.ts
+++ src/copilot-me/Lab02-NorthwindProducts/NorthwindProducts/src/searchApp.ts
@@ -1,12 +1,10 @@
-import { default as axios } from "axios";
-import * as querystring from "querystring";
 import {
   TeamsActivityHandler,
-  CardFactory,
   TurnContext,
   MessagingExtensionQuery,
   MessagingExtensionResponse,
 } from "botbuilder";
+import productSearchME from "./messageExtensions/productSearchME";
 
 export class SearchApp extends TeamsActivityHandler {
   constructor() {
@@ -18,46 +16,13 @@ export class SearchApp extends TeamsActivityHandler {
     context: TurnContext,
     query: MessagingExtensionQuery
   ): Promise<MessagingExtensionResponse> {
-    const searchQuery = query.parameters[0].value;
-    const response = await axios.get(
-      `http://registry.npmjs.com/-/v1/search?${querystring.stringify({
-        text: searchQuery,
-        size: 8,
-      })}`
-    );
 
-    const attachments = [];
-    response.data.objects.forEach((obj) => {
-      const adaptiveCard = CardFactory.adaptiveCard({
-        $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
-        type: "AdaptiveCard",
-        version: "1.4",
-        body: [
-          {
-            type: "TextBlock",
-            text: `${obj.package.name}`,
-            wrap: true,
-            size: "Large",
-          },
-          {
-            type: "TextBlock",
-            text: `${obj.package.description}`,
-            wrap: true,
-            size: "medium",
-          },
-        ],
-      });
-      const preview = CardFactory.heroCard(obj.package.name);
-      const attachment = { ...adaptiveCard, preview };
-      attachments.push(attachment);
-    });
+    const meName = query.parameters[0].name;
+    switch (meName) {
+      case "searchQuery": {
+        return productSearchME.handleTeamsMessagingExtensionQuery(context, query);
+      }
+    }
 
-    return {
-      composeExtension: {
-        type: "result",
-        attachmentLayout: "list",
-        attachments: attachments,
-      },
-    };
   }
 }
